39ed00c19bd73deeda96e0c3f5b77400454a6eb8
藤田 浩輔 <e1211033@yahoo.co.jp>
2021-08-17 11:27:26 +0900

    デバッグ用var_dumpおよびtest用商品画像削除

--- www/view/cart_view.php
+++ www/view/cart_view.php
@@ -6,9 +6,6 @@
   <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . 'cart.css'); ?>">
 </head>
 <body>
-  <?php var_dump($carts); ?>
-  <?php var_dump($user); ?>
-  <?php var_dump($order_id); ?>
   <?php include VIEW_PATH . 'templates/header_logined.php'; ?>
   <h1>カート</h1>
   <div class="container">
